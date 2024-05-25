d254651b4041d2db039ca5bb93b887370a82d9c3
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2024-05-25 00:52:11 -0500

    store image uploaded by user

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,8 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
+import sharp from 'sharp'
 import { z } from 'zod';
+import { db } from '@/db'
 
 const f = createUploadthing();
 
@@ -12,8 +14,36 @@ export const ourFileRouter = {
       return { input };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-     const { configId } =  metadata.input
-      return { configId };
+      const { configId } =  metadata.input
+
+      const res = await fetch(file.url)
+      const buffer = await res.arrayBuffer()
+
+      const imgMetadata = await sharp(buffer).metadata()
+      const { width, height } = imgMetadata
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        })
+
+        return { configId: configuration.id }
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        })
+
+        return { configId: updatedConfiguration.id }
+      }
     }),
 } satisfies FileRouter;
 
